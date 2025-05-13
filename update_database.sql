60e0dc52a53a1cca45241b7add3649bfb64be544
Phanly <buon0a2003@gmail.com>
2025-05-13 10:03:03 +0700

    reset pass

--- update_database.sql
+++ update_database.sql
@@ -2,7 +2,3 @@
 ALTER TABLE doanvien 
 ADD COLUMN reset_token VARCHAR(255) NULL,
 ADD COLUMN reset_token_expiry DATETIME NULL;
-
--- Make sure the email column exists
-ALTER TABLE doanvien 
-ADD COLUMN email VARCHAR(255) NULL; 
