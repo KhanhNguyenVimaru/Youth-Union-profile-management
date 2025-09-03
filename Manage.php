12e0858beb292c493d35e3553ecec7f77c7852ba
KhanhNguyenVimaru <khanhnd05@gmail.com>
2025-09-03 21:01:21 +0700

    connect modal

--- Manage.php
+++ Manage.php
@@ -95,6 +95,7 @@ if (!isset($_SESSION['role']) || ($_SESSION['role'] !== 'admin')) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 <script src="index.js"></script>
 <script src="manage_user.js"></script>
+<script src="modal_handlers.js"></script>
 <script>
     function resetPage() {
         window.location.reload();
